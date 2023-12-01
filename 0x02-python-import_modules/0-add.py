b1b1e0823f6832cbb1e99f0d300fb63082e88951
Soufiane dahhani_ <dahhani.yane@gmail.com>
2023-12-01 02:47:25 -0800

    ex 1 project python 4

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
-a = 1
-b = 2
+#!/usr/bin/python3
 
-exec_str = "from add import add"
-exec(exec_str)
+if __name__ == "__main__":
+    
+    from add import add
 
-result = add(a, b)
-print('{} + {} = {}'.format(a, b, result))
+    a = 4
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
